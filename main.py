827b3d283704c97ec6a912b6b34c5f456c54fc61
Lesona-Systems <nejwritings@gmail.com>
2022-10-20 16:57:06 -0500

    i have no idea

--- main.py
+++ main.py
@@ -1,14 +1,10 @@
-# Copyright © 2022 Nicholas Johnson
-
-# Permission to use, copy, modify, distribute, and sell this software and its
-# documentation for any purpose is hereby granted without fee, provided that
-# the above copyright notice appear in all copies and that both that
-# copyright notice and this permission notice appear in supporting
-# documentation.  No representations are made about the suitability of this
-# software for any purpose.  It is provided "as is" without express or
-# implied warranty.
-
-import os, zipfile, webbrowser, shutil, json
+import configparser
+import os
+import zipfile
+import webbrowser
+import shutil
+import json
+import configparser
 from selenium import webdriver
 from selenium.webdriver import FirefoxOptions
 from selenium.webdriver.common.by import By
