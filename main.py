f27ea357c19c367ede62e5b2afaac9539f49bf75
Lesona-Systems <nejwritings@gmail.com>
2022-11-04 18:07:26 -0500

    implement ElvUI version checking

--- main.py
+++ main.py
@@ -4,10 +4,12 @@ import webbrowser
 import shutil
 import json
 import configparser
+from time import sleep, time
 from selenium import webdriver
 from selenium.webdriver import FirefoxOptions
 from selenium.webdriver.common.by import By
-from time import sleep, time
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 class colors:
     GREEN = '\033[92m'
@@ -61,13 +63,18 @@ def main():
             current_version_time = get_update_time(name['anchor_link'], ublock_xpi_path)
             if current_version_time != name['last_updated']:
                 url_list.append(name['dl_url'])
-                to_be_updated.append(name)
+                to_be_updated.append(key)
                 name['last_updated'] = current_version_time
             else:
                 continue
-        # elif name['location'] == 'tukui':
-        #     print(f'Processing {key}...')
-        #     current_version_time = get_version_tukui(name['anchor_link'], ublock_xpi_path)
+        elif name['location'] == 'elvui':
+            print(f'Processing {key}...')
+            current_version = get_version_elvui(name['anchor_link'], ublock_xpi_path)
+            if current_version != name['version']:
+                dl_url = (f"{name['dl_url']}{current_version}.zip")
+                url_list.append(dl_url)
+                to_be_updated.append(key)
+                name['version'] = current_version
         else:
             print(f'Processing {key}...')
             url_list.append(name['dl_url'])
@@ -252,22 +259,19 @@ def get_update_time(url, ublock_xpi_path):
 
     return last_updated
 
-# def get_version_tukui(url, ublock_xpi_path):
-#     browser = start_browser()
+def get_version_elvui(url, ublock_xpi_path):
+    driver = start_browser()
 
-#     if os.name == 'nt':
-#         ublock = fr"{ublock_xpi_path}"
-#     else:
-#         ublock = ublock_xpi_path
+    driver.install_addon(ublock_xpi_path)
+    driver.get(url)
 
-#     browser.install_addon(ublock)
-#     browser.get(url)
+    version = [my_elem.get_attribute("innerText") for my_elem in WebDriverWait(driver, 20).until(EC.visibility_of_all_elements_located((By.XPATH, "/html/body/div[2]/div/div/ul/li/div[4]/div/a[2]/span")))]
 
-#     xpath = browser.find_element(By.XPATH, "//b[@class='Premium']")
-#     last_updated = xpath.text
-#     browser.close()
+    version = (version[0])
+    
+    driver.close()
 
-#     return last_updated
+    return version
 
 if __name__ == '__main__':
     main()
