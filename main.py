bf57ffcfb46786528ed6ed16dd755bfdffe836ae
Lesona-Systems <nejwritings@gmail.com>
2023-06-15 17:19:37 -0500

    fix for https://github.com/Lesona-Systems/Underwolf/issues/19

--- main.py
+++ main.py
@@ -319,11 +319,10 @@ def get_version_elvui(elv_url, ublock_xpi_path):
     # Wait for ElvUI version number to be visable, then grab it from tukui.org/welcome.php
     # There's always the possibility that Tukui changes the location of the version number.
     # If that happens, the XPATH below will break.
-    current_version = [my_elem.get_attribute("innerText") for my_elem in WebDriverWait(driver, 20).until(EC.visibility_of_all_elements_located((By.XPATH, "/html/body/div[2]/div/div/ul/li/div[4]/div/a[2]/span")))]
-
-    # the above returns a list by default, so reassign "version" to the the first element in the list
+    current_version = [my_elem.get_attribute("innerText") for my_elem in WebDriverWait(driver, 20).until(EC.visibility_of_all_elements_located((By.XPATH, '//*[@id="download-button"]')))]
+    # the above returns a list by default, so reassign "version" to the the first element in the list.
     current_version = (current_version[0])
-    
+
     driver.close()
 
     return current_version
